61234f25d99b64bab2ca7f1eb520e35f06ccc2d5
KachiYD <kachi.oragui@gmail.com>
2024-05-21 13:18:30 -0600

    Added dyamic metadata to meals details pages

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -5,6 +5,19 @@ import classes from './page.module.css'
 import Image from 'next/image'
 import { notFound } from 'next/navigation';
 
+export async function generateMetadata( {params} ) {
+    const meal = getmeal(params.mealSlug)
+
+    if (!meal) {
+        notFound();
+    }
+
+    return {
+        title: meal.title,
+        description: meal.summary
+    };
+}
+
 export default function MealDetailsPage( {params} ) {
     const meal = getmeal(params.mealSlug)
 
